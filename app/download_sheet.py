80e62bf15f6a7730ef15c3a368dbd115a1227f0f
GlassCanon91 <vuquanghuy12@gmail.com>
2021-02-23 16:19:35 +0700

    zz

--- app/download_sheet.py
+++ app/download_sheet.py
@@ -168,7 +168,7 @@ app = Flask(__name__)
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/png')#'image/vnd.microsoft.icon'
 
 @app.route('/')
 def my_form():
